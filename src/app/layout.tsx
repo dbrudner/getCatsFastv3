16a0e3cb6cdf0f5f10f9f7fd3f111dc74069304f
Dave Brudner <dave@gridline.co>
2024-06-16 03:48:13 -0500

    nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,19 @@ import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, SignOutButton, SignedIn } from "@clerk/nextjs";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import { ThemeProvider } from "@mui/material";
+import { IconButton, ThemeProvider } from "@mui/material";
 import GetCatsFastThemeProvider from "./theme";
+import Link from "next/link";
+import {
+  ArrowRightEndOnRectangleIcon,
+  HomeIcon,
+  PlusIcon,
+} from "@heroicons/react/24/outline";
+import Head from "next/head";
+import classNames from "classnames";
+import Nav from "@/components/nav/nav";
 
 const workSans = Work_Sans({ subsets: ["latin"] });
 
@@ -21,13 +30,20 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
       <AppRouterCacheProvider>
         <GetCatsFastThemeProvider>
           <html lang="en">
             <body
-              className={`${workSans.className} min-h-screen bg-black text-white`}
+              className={`${workSans.className} min-h-screen bg-black text-white relative`}
             >
               {children} <Analytics />
+              <Nav />
             </body>
           </html>
         </GetCatsFastThemeProvider>
---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,44 @@
+import { SignOutButton } from "@clerk/nextjs";
+import {
+  HomeIcon,
+  PlusIcon,
+  ArrowRightEndOnRectangleIcon,
+} from "@heroicons/react/24/outline";
+import { IconButton } from "@mui/material";
+import classNames from "classnames";
+import Link from "next/link";
+
+export default function Nav() {
+  const bottomMobileNavClassNames =
+    "fixed bottom-0 left-0 w-screen flex flex-row justify-around p-4 bg-black border-indigo-500 border-t-2";
+
+  const sideNavClassNames =
+    "lg:start-auto lg:flex-col lg:gap-y-4 lg:h-screen lg:top-0 lg:left-0 lg:w-24 lg:items-center lg:border-t-0 lg:justify-start lg:border-r-2";
+
+  const navClassNames = classNames(
+    sideNavClassNames,
+    bottomMobileNavClassNames
+  );
+
+  return (
+    <div className={navClassNames}>
+      <Link href="/cats">
+        <IconButton>
+          <HomeIcon className="w-10 h-10" />
+        </IconButton>
+      </Link>
+      <Link href="/cat/new">
+        <IconButton>
+          <PlusIcon className="w-10 h-10" />
+        </IconButton>
+      </Link>
+      <SignOutButton redirectUrl="/cats">
+        <div>
+          <IconButton>
+            <ArrowRightEndOnRectangleIcon className="w-10 h-10" />
+          </IconButton>
+        </div>
+      </SignOutButton>
+    </div>
+  );
+}
