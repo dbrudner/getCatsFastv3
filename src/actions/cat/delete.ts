3e4d6aac0d17fc9f480ada5fbafb3ca906ac0360
Dave Brudner <dave@gridline.co>
2024-06-22 16:00:59 -0500

    stuff

--- src/actions/cat/delete.ts
+++ src/actions/cat/delete.ts
@@ -1,16 +1,28 @@
 "use server";
 
 import { CatsTable, db } from "@/lib/drizzle";
-import { eq } from "drizzle-orm";
+import { currentUser } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
 
 export async function deleteCat(catId: number): Promise<void> {
   console.log("Deleting cat with id", catId);
+
+
   try {
+    const resolvedCurrentUser = await currentUser();
+
+    const resolvedCurrentUserId = resolvedCurrentUser?.id;
+
+    if (!resolvedCurrentUserId) {
+      throw new Error("No current user");
+    }
+
     const deletedCats = await db
       .delete(CatsTable)
-      .where(eq(CatsTable.id, catId));
-    // const deletedCat = deletedCats[0];
-    // console.log("Deleted cat", deletedCat);
+      .where(and(
+        eq(CatsTable.id, catId),
+        eq(CatsTable.userId, resolvedCurrentUser.id)
+      ));
   } catch (e) {
     console.error("Error deleting cat", e);
     throw e;
