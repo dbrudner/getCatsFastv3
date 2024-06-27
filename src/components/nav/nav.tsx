f2e446ecd2f8c236e3b0ba1447d3ed26ce243fe3
Dave Brudner <dave@gridline.co>
2024-06-27 08:36:12 -0500

    Nav fixes

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { HomeIcon, PlusIcon } from "@heroicons/react/24/outline";
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const baseNavItemClassnames = "w-8 h-8 text-slate-600";
+const baseNavItemClassnames = "text-slate-600";
+const baseNavIconButtonClassnames = "w-10 h-10";
 
 type NavItemProps = {
   path: string;
@@ -13,43 +14,50 @@ type NavItemProps = {
 };
 
 function NavItem({ path, children }: NavItemProps) {
-  return (
-    <Link href={path}>
-      <IconButton>{children}</IconButton>
-    </Link>
-  );
+  return <Link href={path}>{children}</Link>;
 }
 
 export default function Nav() {
   const pathname = usePathname();
 
-  function highlightIconIfActive(path: string) {
+  function highlightNavItemIfActive(path: string) {
     return classNames(baseNavItemClassnames, {
       "text-white": pathname === path,
     });
   }
 
+  function highlightNavIconButtonIfActive(path: string) {
+    return classNames(
+      highlightNavItemIfActive(path),
+      baseNavIconButtonClassnames,
+    );
+  }
+
   const bottomMobileNavClassNames =
-    "fixed bottom-0 left-0 w-screen flex flex-row justify-around p-4 bg-black border-indigo-500 border-t-2";
+    "fixed bottom-0 left-0 w-screen flex flex-row items-center justify-around p-4 bg-black border-indigo-500 border-t-2";
 
   const sideNavClassNames =
     "lg:start-auto lg:flex-col lg:gap-y-4 lg:h-screen lg:top-0 lg:left-0 lg:w-24 lg:items-center lg:border-t-0 lg:justify-start fixed";
 
   const navClassNames = classNames(
     sideNavClassNames,
-    bottomMobileNavClassNames
+    bottomMobileNavClassNames,
   );
 
   return (
     <div className={navClassNames}>
       <NavItem path="/cats">
-        <p className={highlightIconIfActive("/cats")}>Cats</p>
+        <Button variant="text" size="small">Cats</Button>
       </NavItem>
       <NavItem path="/">
-        <HomeIcon className={highlightIconIfActive("/")} />
+        <IconButton>
+          <HomeIcon className={highlightNavIconButtonIfActive("/")} />
+        </IconButton>
       </NavItem>
       <NavItem path="/cat/new">
-        <PlusIcon className={highlightIconIfActive("/cat/new")} />
+        <IconButton>
+          <PlusIcon className={highlightNavIconButtonIfActive("/cat/new")} />
+        </IconButton>
       </NavItem>
     </div>
   );
