d64a76c3fd4ad2d97c74a335341e280486030b89
Dave Brudner <dave@gridline.co>
2024-07-04 19:06:48 -0500

    more

--- src/app/utils/tailwind-classes.ts
+++
@@ -1,9 +0,0 @@
-type TailwindClassName = "text-2xl" | "text-3xl" | "text-4xl";
-
-type TailwindClasses = {
-  [tailwindClassName: TailwindClassName]: string;
-};
-
-export default function twCn(): string {
-  return "";
-}
