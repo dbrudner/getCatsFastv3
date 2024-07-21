948d1de5c095ee6929aa2b624529d21d57f855f0
Dave Brudner <dave@gridline.co>
2024-07-21 17:14:51 -0500

    sign in page updates

--- src/app/signin/[[...rest]]/page.tsx
+++ src/app/signin/[[...rest]]/page.tsx
@@ -1,21 +1,31 @@
 import { SignIn } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Box } from "@mui/material";
 
 export default function Signin() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <SignIn
-        appearance={{
-          baseTheme: dark,
-          elements: {
-            card: "bg-slate-900",
-            formButtonPrimary: `bg-black text-white font-bold`,
-          },
-          variables: {
-            borderRadius: "5px",
-          },
-        }}
-      />
-    </div>
+    <Box sx={{ ".cl-footer": { background: "none" } }}>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <SignIn
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              rootBox: "min-w-screen max-w-md w-full border-none",
+              headerTitle: "text-4xl tracking-wider text-left",
+              headerSubtitle: "text-left tracking-wide",
+              card: "bg-green rounded-none border-none bg-gradient-to-r from-slate-950 to-slate-800",
+              cardBox: "rounded-none w-full border-none shadow-none",
+              formButtonPrimary: `bg-black text-white font-bold`,
+              footer: "justify-start items-start",
+              footerAction: "justify-start",
+              footerActionLink: "text-fuchsia-300 font-bold text-left ml-1",
+            },
+            variables: {
+              borderRadius: "5px",
+            },
+          }}
+        />
+      </div>
+    </Box>
   );
 }
